4bc48d9d6c6c190069ba931c4c8152821bae57c0
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-11-15 16:32:40 +0500

    Create exception.h

---
+++ exception.h
@@ -0,0 +1,27 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+#include <exception>
+
+class Exception {
+    public:
+
+    class UnknownCoord : public std::exception {
+        const char * what() const throw() {
+            return "Coordinates of the object are unknown";
+        }
+    };
+
+    class UnknownVelocity : public std::exception {
+        const char * what() const throw() {
+            return "Velocity of the object are unknown";
+        }
+    };
+
+    class UnknownTimeStep : public std::exception {
+        const char * what() const throw() {
+            return "Time step are unknown";
+        }
+    };
+};
+
+#endif // EXCEPTION_H
